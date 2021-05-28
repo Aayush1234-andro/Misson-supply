effaca349e409b08d46f59bcd1e5eb959fd75c0c
Aayush1234-andro <79866727+Aayush1234-andro@users.noreply.github.com>
2021-05-28 23:10:53 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,33 @@
-
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
 var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+var e1, e2,e3;
+var Y=1;
+var N=0;
+var state=Y;
+
 
 function preload()
 {
 	helicopterIMG=loadImage("helicopter.png")
 	packageIMG=loadImage("package.png")
+
 }
 
 function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
 	
+	e1=createSprite(360,650,200,15,{isStatic:true});
+	e2=createSprite(265,600,15,100,{isStatic:true});
+	e3=createSprite(455,600,15,100,{isStatic:true});
+
+	e1.shapeColor="red";
+	e2.shapeColor="red";
+	e3.shapeColor="red";
 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
@@ -32,35 +44,14 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, packageBody);
 	
+
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
  	World.add(world, ground);
-
- 	boxPosition=width/2-100
- 	boxY=610;
-
-
- 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
- 	boxleftSprite.shapeColor=color(255,0,0);
-
- 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
- 	World.add(world, boxLeftBody);
-
- 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
- 	boxBase.shapeColor=color(255,0,0);
-
- 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
- 	World.add(world, boxBottomBody);
-
- 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
- 	boxleftSprite.shapeColor=color(255,0,0);
-
- 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
- 	World.add(world, boxRightBody);
 
 
 	Engine.run(engine);
@@ -71,23 +62,48 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
- 
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
+  packageSprite.x= packageBody.position.x ;
+  packageSprite.y= packageBody.position.y ;
 
-  
+
+ packageSprite.collide( e1);
+ packageSprite.collide( e2);
+ packageSprite.collide( e3);
+
+if(packageSprite.isTouching(e1)){
+	state=N;
+}
+if(packageSprite.isTouching(e2)){
+	state=N;
+}
+if(packageSprite.isTouching(e3)){
+	state=N;
+}
+
+ if(state===Y){
+	packageSprite.x=helicopterSprite.x;
+ if(keyDown("right")){
+	helicopterSprite.x=helicopterSprite.x+10;
+ }
+ if(keyDown("left")){
+	helicopterSprite.x=helicopterSprite.x-10;
+ }
+}
+
+
   drawSprites();
-  
-  
  
+
 }
 
 
 function keyPressed() {
-	if (keyCode === DOWN_ARROW) {
-	   // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
-	   Matter.Body.setStatic(packageBody,false);
-	   
-	 }
-   }
-   
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+    Matter.Body.setStatic(packageBody,false);
+   // packageSprite.velocityY=-2;
+  }
+}
+
+
+
